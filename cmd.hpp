db5cf7e732a0521e49ee506badb738c15ef8d349
SgtxRootbeer <christianleone100@gmail.com>
2026-04-25 13:08:44 -0500

    Update cmd.hpp
    editing per request

--- cmd.hpp
+++ cmd.hpp
@@ -10,7 +10,7 @@ struct t_timestamp {
 struct t_move {
     int16_t deltaX;
     int16_t deltaY;
-    bool symmetrical;
+    bool symmetrical = false; //setting false by default as a safety thing
 };
 
 struct t_crop {
@@ -246,11 +246,10 @@ bool parseCMD(int32_t argc, char** argv, t_cmd& cmd) {
             if (remaining < 2) return false;
             cmd.move.deltaX = atoi(argv[i++]);
             cmd.move.deltaY = atoi(argv[i++]);
-            cmd.move.symmetrical = false;
-            if (remaining >= 3 && std::string(argv[i]) == "-s") {
-                cmd.move.symmetrical = true;
-                i++;
-            }
+        }
+        else if (arg == "symmetrical" || arg == "--symmetrical") {
+            cmd.move.symmetrical = true;
+            i++;
         }
         else if (arg == "crop" || arg == "--crop") {
             if (remaining < 4) return false;
